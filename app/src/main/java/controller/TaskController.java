9b9b11b1c0986dfb636186671ac411a77733de88
WillianDomingues79 <willian.domingues3@gmail.com>
2023-03-19 14:58:22 -0300

    feat:Conexão inicial com BD MySql

---
+++ app/src/main/java/controller/TaskController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.List;
+import model.Task;
+
+public class TaskController {
+    
+    public void save(Task task){
+        
+    }
+    
+    public void update(Task task){
+        
+    }
+    
+    public void removeById(int taskId){
+        String sql = "DELETE FROM tasks WHERE id = ?";
+        
+        Connection conn = null;
+        PreparedStatement statement = null;
+        
+        try {
+            
+        } catch (Exception e) {
+        }
+    }
+    
+    public List<Task> getAll(int idProject){
+        return null;
+    }
+    
+}
